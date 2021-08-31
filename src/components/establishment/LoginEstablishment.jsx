dd55e68c496f7b407b8cb3477e3bb7445cfdf43b
Walisson <40742446+WalissonCF@users.noreply.github.com>
2021-08-31 19:08:40 -0300

    MSE-003
    Tela de login para estabelecimentos criada e criada nova rota para navegação até o login

---
+++ src/components/establishment/LoginEstablishment.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+
+import axios from 'axios';
+import { Route, Link } from 'react-router-dom';
+
+const ESTABLISHMENT_LOGIN_URL = 'https://my-safe-establishment.herokuapp.com/public/owner/login'
+export default class LoginEstablishment extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            email: '',
+            password: '',
+        };
+    }
+
+    onChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        const { email, password} = this.state;
+
+        axios.post(ESTABLISHMENT_LOGIN_URL, { email, password })
+            .then((res) => {
+                console.log(res);
+                console.log(res.data);
+                console.log(res.status);
+            });
+    }
+
+    render() {
+        const { email, password } = this.state;
+        const linkRegisterEstablishment = "/register-establishment";
+
+        return (
+            <React.Fragment>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="content-menu-register">
+                                <div class="content-in-text">
+                                    <h1>Bem-vindo ao</h1>
+                                    <h2>My safe establishment web</h2>
+                                    <p>
+                                    Esse ambiente é privado ao estabelecimento <br /> <br /> <br /> <br /> 
+                                    Essa aplicação é destinada a segurança <br />
+                                    do seu estabelecimento <br /> <br /> 
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <form onSubmit={this.onSubmit}>
+                        <div class="form-group">
+                            <label for="email">E-mail:</label>
+                            <input type="email" class="form-control" id="email-login-register-establishment"
+                            name="email" value={email} onChange={this.onChange} required />
+                        </div>
+                        <div class="form-group">
+                            <label for="password-register-establishment">Senha:</label>
+                            <input type="password" class="form-control" id="password-register-establishment"
+                            name="password" value={password} onChange={this.onChange} 
+                            required />
+                        </div>
+                        <button type="submit" class="btn btn-outline-danger">ENTRAR</button>
+                        <h6>Cadastre-se sua <Link to={linkRegisterEstablishment}>empresa</Link>!</h6>
+                    </form>
+                    <hr />
+                    <h6 class="responsible">Desenvolvido por alunos da Universidade Paulista(UNIP) - 2021</h6>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import LoginUser from './components/user/Login';
 import RegisterUser from './components/user/RegisterUser';
+import LoginEstablishment from './components/establishment/LoginEstablishment';
 import RegisterEstablishment from './components/establishment/RegisterEstablishment';
 
 import { Route, Routes } from 'react-router-dom';
@@ -12,7 +13,7 @@ export default function MainRoutes() {
             <Route path="/" element={ <LoginUser /> } />
             <Route path="/register" element={ <RegisterUser /> } />
 
-            <Route path="/login-establishment" element= { <h1>VAI EXPLOOOOODIR!!!</h1> } />
+            <Route path="/login-establishment" element= { <LoginEstablishment /> } />
             <Route path="/register-establishment" element={ <RegisterEstablishment />} />
         </Routes>
     );
