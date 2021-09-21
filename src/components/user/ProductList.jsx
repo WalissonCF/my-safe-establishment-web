93870a92d2389f235a392fdd4c37425ad574eca9
Walisson <40742446+WalissonCF@users.noreply.github.com>
2021-09-21 18:48:23 -0300

    MSE-004
    Adicionada novas imagens de exemplos para o cardapio

--- src/components/user/ProductList.jsx
+++ src/components/user/ProductList.jsx
@@ -13,11 +13,15 @@ export default class ProductList extends React.Component {
     }
 
     menu() {
-        const srcs = ["https://miro.medium.com/max/1838/1*CgccG6m3Xl8s2EJI6_e7sw.png",
-        "https://criticalhits.com.br/wp-content/uploads/2021/03/AAAABYQp33Z3D9uGJK0IZsYfvENQpSz4zoSrjb8v5CCl4UTiFDe7Z_yovhieDFhJtGm2Rh4LoleJfHwHdyRDMtezwqojkDXH.jpg",
-        "https://narutokonoha.com/wp-content/uploads/2019/10/Imagens-do-anime-Naruto-para-Imprimir-e-Colorir.jpg",
-        "https://images.unsplash.com/photo-1516123359062-32456af7c128?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80"];
-        const names = ["Programação", "Naruto", "Naruto2", "Flor"]
+        const srcs = ["https://skdesu.com/wp-content/uploads/2017/10/shokugeki.jpg",
+        "https://shokugekinosoumafoodwars.files.wordpress.com/2018/09/974c191f7974357d204fc1cd38157e744f17c169_hq.gif?w=778",
+        "https://shokugekinosoumafoodwars.files.wordpress.com/2018/09/transforming_furikake_shining.gif?w=778",
+        "https://shokugekinosoumafoodwars.files.wordpress.com/2018/09/rainbow_terrine_anime.png?w=778",
+        "https://shokugekinosoumafoodwars.files.wordpress.com/2018/09/chou_farci_anime.png?w=778",
+        "https://shokugekinosoumafoodwars.files.wordpress.com/2018/09/eggs_benedict_anime.png?w=778",
+        "https://shokugekinosoumafoodwars.files.wordpress.com/2018/09/oja-style_pacific_saury_takikomi_gohan_anime.png?w=778"];
+        const names = ["Bacon assado", "Sumire Karaage Roll", "Furikake Gohan", "Terrine Arco-íris", "Chou Farci",
+        "Ovos Benedict", "Sahi Pacífico"]
 
         let list = [
             {
@@ -47,7 +51,7 @@ export default class ProductList extends React.Component {
                         </div>
                     </div>
                     <form>
-                        <h1 className="menu">Faça seu pedido:</h1>
+                        <h1 className="menu">Faça seu pedido</h1>
                         <h2 className="menu-1">Mesa: {table}</h2>
                         <h2 className="menu-2">Carpadio:</h2>
                         {
