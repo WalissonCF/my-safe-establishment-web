5d34f3f41ab602c5b32852e581505b0afbe1529c
Walisson <40742446+WalissonCF@users.noreply.github.com>
2021-10-12 19:11:57 -0300

    MSE-004
    Refatorada classe onde é apresentado os produtos para que seja salva  response

--- src/components/user/Tables.jsx
+++ src/components/user/Tables.jsx
@@ -4,10 +4,10 @@ import '../../styles/amountOfPeopleUser.css';
 import userService from '../../services/UserService';
 
 function Table() {
-    const [ posts, setPosts ] = useState([]);
+    const [posts, setPosts] = useState([]);
     const productList = "/product-list";
 
-    async function fetchPosts () {
+    async function fetchPosts() {
         console.log("Batendo na API...");
         await userService.getTables().then(setPosts);
     }
@@ -31,42 +31,42 @@ function Table() {
             <form action="">
                 <div className="form-group" id="select-table">
                     <h2>Por favor selecione <br /> uma mesa</h2>
-                        <div className="background-color">
-                            {
-                                posts.map((item) => {
-                                    const ids = [item.id];
-                                    const statusTables = [item.statusTable];
-                                    let tables = [
-                                        {
-                                            id: ids,
-                                            statusTable: statusTables,
+                    <div className="background-color">
+                        {
+                            posts.map((item) => {
+                                const ids = [item.id];
+                                const statusTables = [item.statusTable];
+                                let tables = [
+                                    {
+                                        id: ids,
+                                        statusTable: statusTables,
+                                    }
+                                ];
+                                return tables.map((itens) => {
+                                    const status = itens.statusTable.map((situations) => {
+                                        if (situations === '0') {
+                                            return "tables";
+                                        } else {
+                                            return "tables-1";
                                         }
-                                    ];
-                                    return tables.map((itens) => {
-                                        const status = itens.statusTable.map((situations) => {
-                                            if (situations === '0') {
-                                                return "tables";
-                                            } else {
-                                                return "tables-1";
-                                            }
-                                        })
-                                        return itens.id.map((t, index) => {
-                                            if ([status[index]].toString() === "tables") {
-                                                return <div key={index} className={[status[index]]} onClick={onClickCheckTable}><p className="number-table">Mesa {t}</p></div>
-                                            } else {
-                                                return <div key={index} class={[status[index]]}><p className="number-table">Mesa {t}</p></div>
-                                            }
-                                        })
+                                    })
+                                    return itens.id.map((t, index) => {
+                                        if ([status[index]].toString() === "tables") {
+                                            return <div key={index} className={[status[index]]} onClick={onClickCheckTable}><p className="number-table">Mesa {t}</p></div>
+                                        } else {
+                                            return <div key={index} class={[status[index]]}><p className="number-table">Mesa {t}</p></div>
+                                        }
                                     })
                                 })
-                            }
-                            <div>
-                                <h2 id="table-selected"></h2>
-                                <Link to={productList}>
-                                    <button type="submit" class="btn btn-outline-danger btn-quantity-customer">CONTINUAR</button>
-                                </Link>
-                            </div>  
+                            })
+                        }
+                        <div>
+                            <h2 id="table-selected"></h2>
+                            <Link to={productList}>
+                                <button type="submit" class="btn btn-outline-danger btn-quantity-customer">CONTINUAR</button>
+                            </Link>
                         </div>
+                    </div>
                 </div>
             </form>
         </div>
