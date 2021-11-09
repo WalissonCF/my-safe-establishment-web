cd391382ac34e1d443f3c23342b8f42d15a033eb
Walisson <40742446+WalissonCF@users.noreply.github.com>
2021-11-09 20:22:03 -0300

    MSE-004
    Criado novos métodos de validação dos valores

--- src/components/user/PaymentMethod.jsx
+++ src/components/user/PaymentMethod.jsx
@@ -30,7 +30,38 @@ export default class PaymentMethod extends React.Component {
         this.setState({ [name]: value });
     }
 
-    onSubmit = (e) => {
+    onClickYes() {
+        document.getElementById('warning-payment').hidden = "true";
+        document.getElementById('label-warning-payment').hidden = "true";
+        customerUtils.removeHidden('button-payment');
+    }
+
+    onClickNo() {
+        if (document.getElementById('separate-card-payment').offsetParent === null) {
+            document.getElementById('warning-payment').hidden = "true";
+            document.getElementById('label-warning-payment').hidden = "true";
+            customerUtils.removeHidden('separate-card-payment');
+        } else {
+            customerUtils.removeHidden('warning-payment');
+            customerUtils.removeHidden('label-warning-payment');
+            document.getElementById('separate-card-payment').hidden = "true";
+        }
+    }
+
+    onBlurValidateValuePayment() {
+        const value = parseFloat(customerUtils.getTotalValueProduct());
+        const valuePayment = parseFloat(customerUtils.unFormatNumber(document.getElementById('valuePayment').value))
+        console.log('value', value, 'valuePayment', valuePayment);
+        if (valuePayment > value) {
+            customerUtils.removeHidden('alert-payment');
+            document.getElementById('button-payment').hidden = "true";
+        } else {
+            document.getElementById('alert-payment').hidden = "true";
+            customerUtils.removeHidden('button-payment');
+        }
+    }
+
+    onClickPaymentOrderPad(e) {
         e.preventDefault();
         const { number, name, expiry, cvc } = this.state;
         const typeCard = localStorage.getItem('paymentMethod');
@@ -39,7 +70,7 @@ export default class PaymentMethod extends React.Component {
         if (!value) {
             valuePayment = parseFloat(customerUtils.getTotalValueProduct());
         } else {
-            valuePayment = parseFloat(customerUtils.unFormatNumber(value));            
+            valuePayment = parseFloat(customerUtils.unFormatNumber(value));
         }
         console.log(valuePayment)
 
@@ -50,12 +81,8 @@ export default class PaymentMethod extends React.Component {
             nameCard: name,
             flagCard: 'MasterCard'
         }
+        console.log('ta enviando')
         console.log(card)
-        // userService.postPaymentOrderByCard(typeCard, valuePayment, card);
-    }
-
-    onClickPaymentOrderPad() {
-        // userService.postPaymentOrdenPad();
     }
 
     render() {
@@ -72,7 +99,7 @@ export default class PaymentMethod extends React.Component {
                                 number={this.state.number}
                             />
                         </div>
-                        <form id="form-credit-card" onSubmit={this.onSubmit}>
+                        <form id="form-credit-card">
                             <div className="form-group">
                                 <InputMask
                                     mask="9999 9999 9999 9999" maskChar={null}
@@ -99,7 +126,7 @@ export default class PaymentMethod extends React.Component {
                                     mask="99/99" maskChar={null}
                                     type="tel"
                                     name="expiry"
-                                    placeholder="Expiry"
+                                    placeholder="Data de validade"
                                     onChange={this.handleInputChange}
                                     onFocus={this.handleInputFocus}
                                     className="form-control"
@@ -119,21 +146,37 @@ export default class PaymentMethod extends React.Component {
                             <div className="form-group">
                                 <h3 className="total-products">Total: R${customerUtils.getTotalValueProduct()}</h3>
                             </div>
-                            <div className="form-group">
+                        </form>
+                        <div className="payment">
+                            <div className="form-group label-warning-payment" id="label-warning-payment">
+                                <label>Deseja pagar tudo somente em 1 cartão?</label>
+                            </div>
+                            <div className="warning-payment" id="warning-payment">
+                                <div>
+                                    <button className="btn btn-outline-danger" onClick={this.onClickYes}>SIM</button>
+                                </div>
+                                <div>
+                                    <button className="btn btn-outline-danger" onClick={this.onClickNo}>NÃO</button>
+                                </div>
+                            </div>
+                            <div className="form-group" id="separate-card-payment" hidden>
                                 <label>Quanto deseja pagar com esse cartão?</label>
                                 <InputMask mask="R$999,999,999" maskChar={null} id="valuePayment" type="tel"
-                                    className="form-control" placeholder={`R$${customerUtils.getTotalValueProduct()}`}></InputMask>
+                                    className="form-control" placeholder={`R$${customerUtils.getTotalValueProduct()}`}
+                                    onBlur={this.onBlurValidateValuePayment}></InputMask>
+                                <p id="alert-payment" hidden>*Valor superior ao valor total do pedido</p>
                             </div>
                             <div>
                                 <p id="alert">*Em caso de dúvida procurar um atendente</p>
                             </div>
-                            <button className="btn btn-outline-danger" onClick={this.onClickPaymentOrderPad} type="submit">PAGAR</button>
                             <div id="alert-success-payment" hidden>
                                 <AnimationSuccess></AnimationSuccess>
                                 <p id="alert-success-payment-text">*Pago*</p>
                             </div>
-                        </form>
-                        {/* <button className="btn btn-outline-danger payment-method" onClick={this.onClickPaymentOrderPad}>PAGAR</button> */}
+                        </div>
+                        <div id="button-payment" hidden>
+                            <button className="btn btn-outline-danger payment-method" type="submit">PAGAR</button>
+                        </div>
                     </div>
                 </div>
             </div>
